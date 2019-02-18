6a03709fd8cd8bdba2bbd7f9aa6ae27f9f8a03d8
Tom <tkiernan120@gmail.com>
2019-02-18 21:33:21 +0000

    Revert "WIP"
    This reverts commit f8e60b6c417bc138202eb3a425a1fcbe41dfc10c.

--- README.md
+++ README.md
@@ -1,58 +1,29 @@
-# Salsa  [![Open Source Helpers](https://www.codetriage.com/tomkiernan120/sourcream/badges/users.svg)](https://www.codetriage.com/tomkiernan120/sourcream)
+# Guacamole ![CI status](https://img.shields.io/badge/build-passing-brightgreen.svg) [![Open Source Helpers](https://www.codetriage.com/tomkiernan120/sourcream/badges/users.svg)](https://www.codetriage.com/tomkiernan120/sourcream)
 
-Open source, PHP routing enging
+Guacamole is PHP Templating Engine. The purpose of this repo is to create a neat little bit of sortware whilst also encouraging contribution and also to help learn.
 
-## Getting Started
+## Installation
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+### Requirements
+* PHP >= 5.4
 
-### Prerequisites
-
-PHP >= 5.4
-
-### Installing
-
-```
-$ composer require tom.kiernan/salsa
+## Usage
+`$ composer require tom.kiernan/guacamole`
 ```
 
-```
-$Salsa = new Salsa\Salsa( array( "baseRoute" => "test/route/ ) ); // Set base route for woking in sub directories
-
-$Salsa->addRoute( "/", "test" ); // Ouputs "test"
-
-$Salsa->addRoute( "name/:name", function( $name ) { echo $name  } ) // example "name/tomkiernan" will output "tomkiernan"
-
-$Salsa->run(); // actually runs the router;
-
+## Development
 ```
 
-## Deployment
 
-Add additional notes about how to deploy this on a live system
 
-## Built With
 
-* [Composer](http://www.dropwizard.io/1.0.2/docs/) - Composer
-* 
 ## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
-Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
-
-## Versioning
-
-We are still looking to settle on a versioning system (open to suggestions)
-
-## Authors
-
-* **Tom Kiernan** - *Creator* - [Tom Kiernan](composer require tom.kiernan/salsa)
+Please make sure to update tests as appropriate.
 
 ## License
+[GNU GENERAL PUBLIC LICENSE](https://choosealicense.com/licenses/gpl-3.0/)
 
-This project is licensed under the GNU License - see the [LICENSE.md](LICENSE.md) file for details
-
-## Acknowledgments
-
-* Hat tip to anyone whose code was used
-* Inspiration
-* etc
+## Credit
+[https://www.makeareadme.com/](https://www.makeareadme.com/)
--- src/Util.php
+++ src/Util.php
@@ -1,33 +1,88 @@
 <?php
-/**
- * 
- */
-namespace Salsa;
 
+namespace Guacamole;
 
 /**
- * Util / Helper functions
+ * summary
  */
-trait Util
+class Util
 {
     /**
      * summary
      */
-    public static function isHTML( $string )
+    public function __construct()
     {
-   		return ( $string != strip_tags( $string ) );
+
     }
 
-
-    public static function generateUrl( $string )
+    /**
+     * Check if file exists
+     * @param  string $path path to file will accept relative paths
+     * @param  string $ext  optional extension does assume file is php
+     * @return bool       [description]
+     */
+    public static function fileExists( $path,  $ext = "php" )
     {
-    	$url = preg_replace( "/[^a-zA-Z\/-]/", "", strtolower($string) );
-        $url = preg_replace( "/-{2,}/", "-", $url );
-        $url = preg_replace( "/\/{2,}/", "/", $url );
-        if( "/" !== $url ){
-            $url = ltrim( $url, "/" );
+        return (bool)file_exists($path . "." . $ext);
+    }
+
+    /**
+     * Get the contents of a file
+     * @param  string $path path to file will accept relative paths
+     * @param  string $ext  optional extension does assume file is php
+     * @return mixed        returns files contents or false if file not found;
+     */
+    public static function getFileContents( $path, $ext = "php" )
+    {
+        if (self::fileExists($path, $ext)) {
+            return file_get_contents("{$path}.{$ext}");
         }
-    	return $url;	
+        return false;
     }
-    
+
+    /**
+     * check if variable is a string
+     * @param  mixed  $variable  mixed variable to check
+     * @return boolean           returns boolean
+     */
+    public static function isString( $variable )
+    {
+        return (bool)is_string($variable);
+    }
+
+    /**
+     * check if variable is array
+     * @param  miaxed  $variable  mixed variable to check
+     * @return boolean            returns boolean
+     */
+    public static function isArray( $variable )
+    {
+        return (bool)is_array($variable);
+    }
+
+    /**
+     * find a file in directorty
+     * @param  string $file      file name
+     * @param  string $directory directory path defaults to current working directory
+     * @return string            if fount returns string path to file else return false
+     * @example Util::findFile( 'Tag.php' ); //output './Tag/Tag.php'
+     */
+    public static function findFile( $file, $directory = "." )
+    {
+        $files = scandir($directory);
+
+        foreach ($files as $key => $value) {
+            $path = realpath($directory . DIRECTORY_SEPARATOR . $value);
+
+            if (!is_dir($path)) {
+                if ($file == $value) {
+                    return $path;
+                }
+            } else if ($value != "." && $value != "..") {
+                self::findFile($file, $path);
+            }
+        }
+        return false;
+    }
+
 }
